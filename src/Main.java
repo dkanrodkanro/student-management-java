4c39c14b133aca50c0f51e0b92c69cbb276e0939
dkanrodkanro <thsdh12@naver.com>
2026-02-13 22:36:03 +0900

    first java practice

---
+++ src/Main.java
@@ -0,0 +1,16 @@
+public class Main {
+    public static void main(String[] args) {
+        int a = 10;
+        int b = 3;
+
+        int sum = a + b;
+        int minus = a - b;
+        int multi = a * b;
+        int div = a / b;
+
+        System.out.println("더하기: " + sum);
+        System.out.println("빼기: " + minus);
+        System.out.println("곱하기: " + multi);
+        System.out.println("나누기: " + div);
+    }
+}
